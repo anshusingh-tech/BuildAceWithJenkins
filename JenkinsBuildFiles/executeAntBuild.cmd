62b01ad4d0dd4733a103389ee769974d3db481b4
Anshu Kumar Singh <74443213+singhkanshu@users.noreply.github.com>
2024-07-20 22:18:59 +0530

    added IIB_PATH variable

--- JenkinsBuildFiles/executeAntBuild.cmd
+++ JenkinsBuildFiles/executeAntBuild.cmd
@@ -1,5 +1,5 @@
 echo calling mqsiprofile
-call ${path.iib}\server\bin\mqsiprofile.cmd
+call "%IIB_PATH%\server\bin\mqsiprofile.cmd"
 
 echo calling Ant
 "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\Ant\bin\ant.bat" -buildfile "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\JenkinsBuildFiles\build_main.xml"
