7ba23a47d007a7a537946ab2fa09f056698e8972
Anshu Kumar Singh <74443213+singhkanshu@users.noreply.github.com>
2024-07-20 22:10:36 +0530

    path.iib

--- JenkinsBuildFiles/executeAntBuild.cmd
+++ JenkinsBuildFiles/executeAntBuild.cmd
@@ -1,5 +1,5 @@
 echo calling mqsiprofile
-call ${env.MQSI_BASE_FILEPATH}\server\bin\mqsiprofile.cmd
+call ${path.iib}\server\bin\mqsiprofile.cmd
 
 echo calling Ant
 "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\Ant\bin\ant.bat" -buildfile "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\JenkinsBuildFiles\build_main.xml"
