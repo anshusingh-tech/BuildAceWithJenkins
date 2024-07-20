252f6c122bfc0e7ad5ae02ce2b670a9d19b48a69
anshusingh-tech <ak38338@gmail.com>
2024-07-20 23:29:53 +0530

    testing cmd variable passing

--- JenkinsBuildFiles/executeAntBuild.cmd
+++ JenkinsBuildFiles/executeAntBuild.cmd
@@ -1,5 +1,6 @@
 echo calling mqsiprofile
 call "%IIB_PATH%\server\bin\mqsiprofile.cmd"
+echo %2
 
 echo calling Ant
 "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\Ant\bin\ant.bat" -buildfile "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\JenkinsBuildFiles\build_main.xml"
