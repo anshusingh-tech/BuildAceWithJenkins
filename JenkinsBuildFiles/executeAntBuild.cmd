ee0e347ba365358bbd8a37f8244baac3b8734be6
anshusingh-tech <ak38338@gmail.com>
2024-07-20 23:45:46 +0530

    added again 3

--- JenkinsBuildFiles/executeAntBuild.cmd
+++ JenkinsBuildFiles/executeAntBuild.cmd
@@ -1,6 +1,8 @@
 echo calling mqsiprofile
 call "%IIB_PATH%\server\bin\mqsiprofile.cmd"
 
+set jobname=%1
+
 
 echo calling Ant
-"C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\Ant\bin\ant.bat" -buildfile "C:\ProgramData\Jenkins\.jenkins\workspace\%1\JenkinsBuildFiles\build_main.xml"
+"C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\Ant\bin\ant.bat" -buildfile "C:\ProgramData\Jenkins\.jenkins\workspace\%jobname%\JenkinsBuildFiles\build_main.xml"
