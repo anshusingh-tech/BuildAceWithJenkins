8a40a242a388d42cfb02018706acf61395560e4c
Anshu Kumar Singh <74443213+singhkanshu@users.noreply.github.com>
2024-07-20 21:58:56 +0530

    put path in double quote

--- JenkinsBuildFiles/executeAntBuild.cmd
+++ JenkinsBuildFiles/executeAntBuild.cmd
@@ -1,5 +1,5 @@
 echo calling mqsiprofile
-call C:\Program Files\IBM\ACE\12.0.10.0\server\bin\mqsiprofile.cmd
+call "C:\Program Files\IBM\ACE\12.0.10.0\server\bin\mqsiprofile.cmd"
 
 echo calling Ant
 "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\Ant\bin\ant.bat" -buildfile "C:\ProgramData\Jenkins\.jenkins\workspace\AcefunctionGit\JenkinsBuildFiles\build_main.xml"
